9508b20298ba96d7874062dddf4532e601292c53
Agustin <87640261+agusoler000@users.noreply.github.com>
2024-10-02 21:10:35 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { JoiValidationSchema } from './config/joi.validation';
       rootPath: join(__dirname,'..','public'),
       }),
       MongooseModule.forRoot(process.env.MONGODB,{dbName:'pokemonDb'}), 
+      
     PokemonModule, CommonModule, SeedModule 
   ],
 })
